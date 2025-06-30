25b5084ce4c7602fbe988439dba7257caf126e4b
Binayak Panda <pbinayak604@gmail.com>
2025-06-30 12:07:39 +0530

    created test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+
 dotenv.config();
 
 const app = express();
@@ -13,6 +15,8 @@ mongoose
   .catch((e) => {
     console.log(e);
   });
+
+app.use("/api/user", userRouter);
 
 app.listen(3000, () => {
   console.log("app is listening on port 3000");
