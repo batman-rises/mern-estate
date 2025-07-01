4bfde761f9b10f2504763a8e3928b7dff2b5a2bf
Binayak Panda <pbinayak604@gmail.com>
2025-07-01 08:52:57 +0530

    created middleware & function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -25,3 +25,14 @@ app.use("/api/auth", authRouter);
 app.listen(3000, () => {
   console.log("app is listening on port 3000");
 });
+
+//error handling middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
