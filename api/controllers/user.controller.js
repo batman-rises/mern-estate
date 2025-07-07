512fa61b9b831613a16ee76feefd7a495ad8f161
Binayak Panda <pbinayak604@gmail.com>
2025-07-07 09:13:44 +0530

    cretae update user API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,34 @@
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+
 export const test = (req, res) => {
   res.send("hello");
+};
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only update your own account"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          //$set ke baare me read karna
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...restInfo } = updateUser._doc;
+    res.status(200).json(restInfo);
+  } catch (error) {
+    next(error);
+  }
 };
--- api/index.js
+++ api/index.js
@@ -4,12 +4,15 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import uploadRouter from "./routes/upload.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGO)
